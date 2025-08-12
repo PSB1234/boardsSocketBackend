b6a18cf80faa4aeb91b936e1d18243d899a1b6d4
PSB1234 <prathameshbhoite999s@gmail.com>
2025-08-12 15:56:55 +0530

    build fix 5

--- src/utils/redis-client.ts
+++ src/utils/redis-client.ts
@@ -1,8 +1,11 @@
 import { createClient, type RedisClientType } from "redis";
+import dotenv from "dotenv";
+dotenv.config();
+
 let client: RedisClientType | null = null;
 export async function initializeRedisClient() {
   if (!client) {
-    client = createClient();
+    client = createClient({ url: process.env.REDIS_URL });
     client.on("error", (err) => console.error("Redis Client Error", err));
     client.on("connect", () => console.log("Redis Client Connected"));
     await client.connect();
